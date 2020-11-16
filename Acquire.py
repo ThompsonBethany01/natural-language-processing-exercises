0c56480cfc3a6a0d8d2c5a35a155696dcbcee0a7
Bethany Thompson <thompson.bethany.01@gmail.com>
2020-11-16 11:23:16 -0600

    Update Progress

--- Acquire.py
+++ Acquire.py
@@ -19,6 +19,8 @@ def get_blog_articles(urls, cached = False):
     
     # if we don't have the data or we want to resave with any new data
     else:
+
+        # empty list to add individual blog dictionaries to
         blogs = []
     
         # loops through urls passed in function
@@ -47,7 +49,49 @@ def get_blog_articles(urls, cached = False):
         
     return blogs
 
-    ################################ Acquire Codeup Blog Articles #################################
-    def get_inshorts_data():
+################################ Acquire Codeup Blog Articles #################################
+def get_inshorts_dataset(urls, cached=False):
+    '''
+    Function to scape articles from Inshorts.com; If cached == False, runs code to scrape data
+    from chosen url articles, add to dictionary, save as df in json file. If cached == True,
+    reads the saved json file to a df.
+    '''
+    # if cached, we read already saved json file to df
+    if cached == True:
+        articles = pd.read_json('inshorts_articles.json')
 
+    # cached == False, if we don't have the data or we want to resave with any new data
+    else:
         
+        # empty list to add individual article dictionaries to
+        articles = []
+        
+        # loops through selected articles from Inshorts
+        for article in urls:
+            
+            # dictionary for acrticle and information we are going to find
+            article_dict = {'headline':'','author':'','date':'','article':'','category':''}
+            
+            # web scraping
+            headers = {'User-Agent': 'Inshorts'}
+            data = get(article, headers)
+            # takes URL and returns a soup object of the text
+            soup = BeautifulSoup(data.text)
+
+            # specific article information to add to dictionary
+            article_dict['headline'] = soup.find('span', attrs={"itemprop": "headline"}).string
+            article_dict['author'] = soup.find('span', attrs={"author"}).string
+            article_dict['date'] = soup.find('span', attrs={"date"}).string
+            article_dict['article'] = soup.find('div', attrs={"itemprop": "articleBody"}).string
+            article_dict['category'] = url.split('/')[-2]
+            
+            # adding dictionary to list
+            articles.append(article_dict)
+
+        # converting list of dictionaries to a df
+        articles = pd.DataFrame(articles)
+        articles = articles[['headline', 'author','date','article', 'category']]
+        # Write df to a json file for faster access
+        articles.to_json('inshorts_articles.json')
+        
+    return articles
